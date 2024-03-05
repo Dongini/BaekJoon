59cb8df8aefc2a0f610ad35abd1d7674f6b54186
DongInLee <105210180+Dongini@users.noreply.github.com>
2024-03-05 22:46:36 +0900

    Update Hello World.java

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,8 +1,13 @@
-public class Main{
-    public static void main(String[] args){
-        StringBuilder sb = new StringBuilder();
-        sb.append("Hello World!");
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedWriter buffer = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        System.out.println(sb);
+        buffer.write("Hello World!");
+        buffer.flush(); // 버퍼를 비운다
+        buffer.close(); // I/O 스트림 닫음
     }
 }
